ca761da337b67c96fb9b468d77f160512bdb349d
xd <xingdong.com@live.com>
2019-12-05 17:06:51 +0800

    添加视图定制列表页面静态页面，待添加模拟数据

---
+++ src/views/view/list/ViewList.vue
@@ -0,0 +1,199 @@
+/**
+* 视图列表
+* Author: dong xing
+* Date: 2019/12/5
+* Time: 9:04 上午
+* Email: dong.xing@outlook.com
+*/
+<template>
+  <div class="view-list">
+    <page-view>
+      <a-card :bordered="false">
+        <div class="table-page-search-wrapper">
+          <a-form layout="inline">
+            <a-row :gutter="48">
+              <a-col :md="advanced ? 12 : 8" :sm="24">
+                <a-form-item label="视图ID">
+                  <a-input v-model="queryParam.id" placeholder=""/>
+                </a-form-item>
+              </a-col>
+              <a-col :md="advanced ? 12 : 8" :sm="24">
+                <a-form-item label="视图名称">
+                  <a-input v-model="queryParam.name" placeholder=""/>
+                </a-form-item>
+              </a-col>
+              <template v-if="advanced">
+                <a-col :md="12" :sm="24">
+                  <a-form-item label="视图类型">
+                    <a-select
+                      mode="multiple"
+                      v-model="queryParam.viewType"
+                      placeholder=""
+                    >
+                      <a-select-option v-for="viewType in viewTypes" :key="viewType.value">
+                        {{ viewType.label }}
+                      </a-select-option>
+                    </a-select>
+                  </a-form-item>
+                </a-col>
+              </template>
+              <a-col :md="!advanced && 8 || 24" :sm="24">
+                <span class="table-page-search-submitButtons" :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">
+                  <a-button type="primary" @click="$refs.table.refresh(true)">查询</a-button>
+                  <a-button style="margin-left: 8px" @click="() => queryParam = {}">重置</a-button>
+                  <a @click="toggleAdvanced" style="margin-left: 8px">
+                    {{ advanced ? '收起' : '展开' }}
+                    <a-icon :type="advanced ? 'up' : 'down'"/>
+                  </a>
+                </span>
+              </a-col>
+            </a-row>
+          </a-form>
+        </div>
+
+        <div class="table-operator">
+          <a-button type="primary" icon="plus">新建</a-button>
+          <a-dropdown v-if="selectedRowKeys.length > 0">
+            <a-menu slot="overlay">
+              <a-menu-item key="1"><a-icon type="edit" @click="handleEdit" />编辑</a-menu-item>
+              <a-menu-item key="2"><a-icon type="copy" @click="handleCopy" />复制</a-menu-item>
+              <a-menu-item key="3"><a-icon type="highlight" @click="handleDesign" />设计</a-menu-item>
+              <a-menu-item key="4"><a-icon type="delete" @click="handleDelete"/>删除</a-menu-item>
+            </a-menu>
+            <a-button style="margin-left: 8px">
+              操作 <a-icon type="down" />
+            </a-button>
+          </a-dropdown>
+        </div>
+
+        <s-table
+          ref="table"
+          size="default"
+          rowKey="key"
+          :columns="columns"
+          :data="loadData"
+          :alert="selectedRowKeys.length > 0"
+          :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+          showPagination="auto"
+        >
+          <span slot="serial" slot-scope="text, record, index">
+            {{ index + 1 }}
+          </span>
+          <!-- / 序号 -->
+
+        </s-table>
+      </a-card>
+    </page-view>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+import { STable, Ellipsis } from '@/components'
+import { PageView } from '@/layouts'
+
+export default {
+  name: 'ViewList',
+  components: {
+    PageView,
+    STable,
+    Ellipsis
+  },
+  data () {
+    return {
+      mdl: {},
+      // 高级搜索 展开/关闭
+      advanced: false,
+      // 视图类型
+      viewTypes: [
+        { label: '综合视图', value: 'comprehensive' },
+        { label: '性能配置视图', value: 'performanceConfig' },
+        { label: '拓扑视图', value: 'topology' },
+        { label: '告警视图', value: 'alarm' },
+        { label: '告警配置视图', value: 'alarmConfig' },
+        { label: '性能监控视图', value: 'performanceMonitor' },
+        { label: '三层架构视图', value: 'threeTierArchitecture' },
+        { label: '第三方视图', value: 'thirdParty' },
+        { label: '导航视图', value: 'navigation' },
+        { label: '特殊视图', value: 'special' },
+        { label: '报表视图', value: 'report' },
+        { label: '插件视图', value: 'plugin' }
+      ],
+      // 查询参数
+      queryParam: {},
+      // 表头
+      columns: [
+        {
+          title: '#',
+          scopedSlots: { customRender: 'serial' }
+        },
+        {
+          title: '视图ID',
+          dataIndex: 'id'
+        },
+        {
+          title: '视图标题',
+          dataIndex: 'title',
+          sorter: true
+        },
+        {
+          title: '视图类型',
+          dataIndex: 'types',
+          sorter: true
+        },
+        {
+          title: '视图创建者',
+          dataIndex: 'updatedAt',
+          sorter: true
+        },
+        {
+          title: '缩略图',
+          dataIndex: 'thumbnail'
+        }
+      ],
+      // 加载数据方法 必须为 Promise 对象
+      loadData: parameter => {
+        console.log('loadData.parameter', Object.assign(parameter, this.queryParam))
+        return []
+      },
+      selectedRowKeys: [],
+      selectedRows: []
+    }
+  },
+  methods: {
+    onSelectChange (selectedRowKeys, selectedRows) {
+      this.selectedRowKeys = selectedRowKeys
+      this.selectedRows = selectedRows
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+    },
+    resetSearchForm () {
+      this.queryParam = {
+        date: moment(new Date())
+      }
+    },
+    handleEdit () {
+      console.log('Edit: ', this.selectedRows)
+    },
+    handleCopy () {
+      console.log('Copy: ', this.selectedRows)
+    },
+    handleDesign () {
+      console.log('Design: ', this.selectedRows)
+    },
+    handleDelete () {
+      console.log('Delete: ', this.selectedRows)
+    },
+    handleSub (record) {
+      if (record.status !== 0) {
+        this.$message.info(`${record.no} 订阅成功`)
+      } else {
+        this.$message.error(`${record.no} 订阅失败，规则已关闭`)
+      }
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+</style>
