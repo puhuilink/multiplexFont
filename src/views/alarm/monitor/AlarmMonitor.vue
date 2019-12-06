dde627965ce79d4761154ac0b501e08067d71c8e
lyz <lyz02413@163.com>
2019-12-06 11:16:53 +0800

    .

--- src/views/alarm/monitor/AlarmMonitor.vue
+++ src/views/alarm/monitor/AlarmMonitor.vue
@@ -1,15 +1,61 @@
 <template>
+  <!--S 告警监控 lyz  lyz02413@163.com -->
   <div class="alarm-monitor">
-    <p>alarm monitor</p>
+    <a-card
+      style="width:100%"
+      :tabList="tabList"
+      :activeTabKey="key"
+      @tabChange="key => onTabChange(key, 'key')"
+    >
+      <!-- S- 公共操作区 -->
+      <div slot="headerContent">
+
+      </div>
+
+      <!-- E- 公共操作区 -->
+      {{ key }}
+    </a-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AlarmMonitor'
+  name: 'AlarmMonitor',
+  data () {
+    return {
+      tabList: [
+        {
+          key: 'pending',
+          tab: '待处理告警'
+        },
+        {
+          key: 'confirmed',
+          tab: '已确认告警'
+        },
+        {
+          key: 'shifting',
+          tab: '已前转告警'
+        },
+        {
+          key: 'resolved',
+          tab: '已解决告警'
+        },
+        {
+          key: 'ignore',
+          tab: '已忽略告警'
+        }
+      ]
+    }
+  },
+  methods: {
+    onTabChange (key, type) {
+      console.log(key, type)
+      this[type] = key
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
 
 </style>
