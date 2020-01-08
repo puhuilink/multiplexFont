5c19d54cec8f0cd522b86554d52455c923c97ced
lyz <lyz02413@163.com>
2020-01-08 14:46:27 +0800

    🛴按需引入日历组件，🥩增加巡检管理下日历，任务，路线视图

--- src/mock/index.js
+++ src/mock/index.js
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV !== 'production' || process.env.VUE_APP_PREVIEW === 'tr
   require('./services/quotaList')
   require('./services/analysis')
   require('./services/system')
+  require('./services/patrol')
 
   Mock.setup({
     timeout: 800 // setter delay time
