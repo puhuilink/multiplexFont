8be0aea4c171b0d8a181411fa52ef7e8169d5301
tanghongxin <hongxin.tang@hotmail.com>
2020-02-04 12:02:23 +0800

    :sparkles:(资源模型): 属性列表新增弹窗初始化

---
+++ src/components/Resource/Model/ResourceModelAttrSchema.vue
@@ -0,0 +1,486 @@
+<template>
+  <a-modal
+    centered
+    :confirmLoading="loading"
+    :title="title"
+    v-model="visible"
+    :width="940"
+    wrapClassName="ResourceModelAttrSchema__modal"
+    @cancel="cancel"
+    @afterClose="reset"
+    okText="保存"
+    cancelText="取消"
+  >
+    <a-form
+      :form="form"
+      layout="vertical"
+    >
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="显示名称"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+                { rules: [{ required: true, message: '' }] },
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="属性名称"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+                {
+                  rules: [{ required: true, message: '' }],
+                  // initialValue: 'name'
+                },
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="显示宽度"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="数据类型"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-select
+              v-decorator="[
+                'gender',
+                {
+                  initialValue: 'STRING'
+                },
+              ]"
+            >
+              <a-select-option
+                v-for="item in options.type"
+                :key="item.value"
+                :value="item.value"
+              >{{ item.name }}</a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="显示类型"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-select
+              v-decorator="[
+                'gesnder',
+                {
+                  initialValue: 'TEXT'
+                },
+              ]"
+            >
+              <a-select-option
+                v-for="item in options.displayType"
+                :key="item.value"
+                :value="item.value"
+              >{{ item.name }}</a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="源类型"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="作为查询"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-checkbox
+              v-decorator="[
+                'usernasme',
+              ]"></a-checkbox>
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="非空"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-checkbox
+              v-decorator="[
+                'usernasmse',
+              ]"></a-checkbox>
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="匹配条件"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="源值"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-checkbox
+              v-decorator="[
+                'usernasssme',
+              ]"></a-checkbox>
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="继承"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="隐藏"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-checkbox
+              v-decorator="[
+                'usernasmae',
+              ]"></a-checkbox>
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="不可编辑"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-checkbox
+              v-decorator="[
+                'usernasme1',
+              ]"></a-checkbox>
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="所属分组"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="默认值"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="运算值"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="排序"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="唯一验证"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-checkbox
+              v-decorator="[
+                'usernasme4',
+              ]"></a-checkbox>
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="唯一范围"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="正则验证"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+      </a-row>
+
+      <a-row>
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="提示信息"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-input
+              v-decorator="[
+                'username',
+
+              ]"
+            />
+          </a-form-item>
+        </a-col>
+
+        <a-col :md="12" :span="24">
+          <a-form-item
+            label="资产属性"
+            :label-col="formItemLayout.labelCol"
+            :wrapper-col="formItemLayout.wrapperCol"
+          >
+            <a-checkbox
+              v-decorator="[
+                'usernasme23',
+              ]"></a-checkbox>
+          </a-form-item>
+        </a-col>
+      </a-row>
+    </a-form>
+  </a-modal>
+</template>
+
+<script>
+const formItemLayout = {
+  labelCol: {
+    // span: 6
+  },
+  wrapperCol: {
+    span: 23
+  }
+}
+
+const options = {
+  type: [
+    {
+      name: 'STRING',
+      value: 'STRING'
+    },
+    {
+      name: 'INT',
+      value: 'INT'
+    },
+    {
+      name: 'DOUBLE',
+      value: 'DOUBLE'
+    },
+    {
+      name: 'LONG',
+      value: 'LONG'
+    },
+    {
+      name: 'DATE',
+      value: 'DATE'
+    }
+  ],
+  displayType: [
+    {
+      name: 'TEXT',
+      value: 'TEXT'
+    },
+    {
+      name: 'TEXTAREA',
+      value: 'TEXTAREA'
+    },
+    {
+      name: 'DATE',
+      value: 'DATE'
+    },
+    {
+      name: 'DATETIME',
+      value: 'DATETIME'
+    },
+    {
+      name: 'CHECKBOX',
+      value: 'CHECKBOX'
+    }
+  ]
+}
+
+export default {
+  name: 'ResourceModelAttrSchema',
+  components: {},
+  props: {},
+  data: (vm) => ({
+    form: vm.$form.createForm(vm),
+    formItemLayout,
+    loading: false,
+    options,
+    record: null,
+    title: '',
+    visible: false
+  }),
+  computed: {},
+  methods: {
+    add () {
+      this.title = '新增'
+      this.visible = true
+    },
+    /**
+     * 编辑
+     * @param {Object} record
+     * @return {Undefined}
+     */
+    edit (record) {
+      this.title = '编辑'
+      this.visible = true
+      this.record = {
+        ...record
+      }
+    },
+    cancel () {
+      this.visible = false
+    },
+    reset () {
+      this.form.resetFields()
+      Object.assign(this.$data, this.$options.data.apply(this))
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.ResourceModelAttrSchema {
+  &__modal {
+    .ant-modal-body {
+      height: 415px;
+      overflow: scroll;
+    }
+  }
+}
+</style>
