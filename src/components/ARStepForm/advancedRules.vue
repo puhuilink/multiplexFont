13408dd3f21cdcd464d402bea4900e3a65fa7b2e
lyz <lyz02413@163.com>
2020-02-05 19:58:42 +0800

    🍕添加告警规则分部表单静态modal

--- src/components/ARStepForm/advancedRules.vue
+++ src/components/ARStepForm/advancedRules.vue
@@ -1,69 +1,174 @@
+/*
+ * 规则高级设置
+ */
+
 <template>
   <div>
-    <a-form style="margin: 40px auto 0;">
-      <result title="操作成功" :is-success="true" description="预计两小时内到账" style="max-width: 560px;">
-        <div class="information">
-          <a-row>
-            <a-col :sm="8" :xs="24">付款账户：</a-col>
-            <a-col :sm="16" :xs="24">ant-design@alipay.com</a-col>
-          </a-row>
-          <a-row>
-            <a-col :sm="8" :xs="24">收款账户：</a-col>
-            <a-col :sm="16" :xs="24">test@example.com</a-col>
-          </a-row>
-          <a-row>
-            <a-col :sm="8" :xs="24">收款人姓名：</a-col>
-            <a-col :sm="16" :xs="24">辉夜</a-col>
-          </a-row>
-          <a-row>
-            <a-col :sm="8" :xs="24">转账金额：</a-col>
-            <a-col :sm="16" :xs="24"><span class="money">500</span> 元</a-col>
-          </a-row>
-        </div>
-        <div slot="action">
-          <a-button type="primary" @click="finish">再转一笔</a-button>
-          <a-button style="margin-left: 8px" @click="toOrderList">查看账单</a-button>
-        </div>
-      </result>
+    <a-form :form="form" style="max-width: 500px; margin: 40px auto 0;">
+      <a-form-item
+        label="节点过滤"
+        :labelCol="labelCol"
+        :wrapperCol="wrapperCol"
+      >
+        <a-select
+          allowClear
+          v-decorator="[
+            'node'
+          ]"
+          placeholder="请选择节点过滤"
+        >
+          <a-select-opt-group
+            v-for="(group,index) in screening.CIDomain"
+            :key="index"
+            :label="group.label"
+          >
+            <a-select-option
+              v-for="item in group.options"
+              :key="item.value"
+              :value="item.value"
+            >
+              {{ item.label }}
+            </a-select-option>
+          </a-select-opt-group>
+        </a-select>
+      </a-form-item>
+      <a-form-item
+        label="采集系统"
+        :labelCol="labelCol"
+        :wrapperCol="wrapperCol"
+      >
+        <a-select
+          allowClear
+          v-decorator="[
+            'CIType'
+          ]"
+          placeholder="请选择采集系统"
+        >
+          <a-select-opt-group
+            v-for="(group,index) in screening.CIType"
+            :key="index"
+            :label="group.label"
+          >
+            <a-select-option
+              v-for="item in group.options"
+              :key="item.value"
+              :value="item.value"
+            >
+              {{ item.label }}
+            </a-select-option>
+          </a-select-opt-group>
+        </a-select>
+      </a-form-item>
+      <a-form-item
+        label="告警时间过滤"
+        :labelCol="labelCol"
+        :wrapperCol="wrapperCol"
+      >
+        <a-radio-group
+          name="radioGroup"
+          v-decorator="['alarmTime', {
+            initialValue: 1}
+          ]"
+        >
+          <a-radio :value="1">任意</a-radio>
+          <a-radio :value="2">绝对</a-radio>
+          <a-radio :value="3">相对</a-radio>
+          <a-radio :value="3">重复</a-radio>
+        </a-radio-group>
+      </a-form-item>
+      <a-form-item
+        label="内容关键词"
+        :labelCol="labelCol"
+        :wrapperCol="wrapperCol"
+      >
+        <a-input
+          :style="{width: 'calc(100% - 100px)'}"
+          v-decorator="[
+            'keywords'
+          ]"
+        />
+      </a-form-item>
+      <a-form-item :wrapperCol="{span: 19, offset: 5}">
+        <a-button @click="prevStep">上一步</a-button>
+        <a-button style="margin-left: 8px" type="primary" @click="nextStep">下一步</a-button>
+      </a-form-item>
     </a-form>
   </div>
 </template>
 
 <script>
-import { Result } from '@/components'
-
+import screening from '@/views/alarm/screening'
 export default {
-  name: 'Step3',
-  components: {
-    Result
-  },
+  name: 'AdvancedRules',
   data () {
     return {
-      loading: false
+      screening,
+      labelCol: { lg: { span: 5 }, sm: { span: 5 } },
+      wrapperCol: { lg: { span: 19 }, sm: { span: 19 } },
+      form: this.$form.createForm(this),
+      loading: false,
+      timer: 0,
+      levelList: [
+        'INFO', 'WARNING', 'MINOR', 'MAJOR', 'CRITICAL'
+      ],
+      alarmLevel: []
     }
   },
   methods: {
-    finish () {
-      this.$emit('finish')
+    nextStep () {
+      const { form: { validateFields } } = this
+      // 先校验，通过表单校验后，才进入下一步
+      validateFields((err, values) => {
+        if (!err) {
+          this.$emit('nextStep')
+        }
+      })
     },
-    toOrderList () {
-      this.$router.push('/list/table-list')
+    prevStep () {
+      this.$emit('prevStep')
+    },
+    /**
+     * 告警类型改变
+     */
+    alarmLevelChange (value) {
+      // this.queryParam.alarmType = screening.checkAll(value, this.alarmType)
+      console.log(value)
+      let list = value
+      value.forEach(element => {
+        if (element === 'checkall') {
+          if (length - 1 === this.levelList && value[length - 1] === 'checkall') {
+            list = []
+            console.log('点击')
+          } else {
+            list = []
+            this.levelList.forEach(m => {
+              list.push(m)
+              console.log(list)
+              const a = this.form.getFieldsValue()
+              this.form.setFieldsValue({
+                alarmLevel: list
+              })
+              console.log(a)
+            })
+          }
+        }
+      })
     }
+  },
+  beforeDestroy () {
+    clearTimeout(this.timer)
   }
 }
 </script>
-<style lang="less" scoped>
-  .information {
-    line-height: 22px;
 
-    .ant-row:not(:last-child) {
-      margin-bottom: 24px;
+<style lang="less" scoped>
+  .stepFormText {
+    margin-bottom: 24px;
+
+    .ant-form-item-label,
+    .ant-form-item-control {
+      line-height: 22px;
     }
   }
-  .money {
-    font-family: "Helvetica Neue",sans-serif;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 14px;
-  }
+
 </style>
--- src/components/ARStepForm/rules.vue
+++ src/components/ARStepForm/rules.vue
@@ -64,11 +64,11 @@
       >
         <a-select
           allowClear
+          placeholder="请选择告警类型"
           v-decorator="[
             'alarmType',
             { rules: [{ required: true, message: '请选择告警类型' }] },
           ]"
-          placeholder="请选择告警类型"
         >
           <a-select-opt-group
             v-for="(group,index) in screening.alarmType"
@@ -85,9 +85,34 @@
           </a-select-opt-group>
         </a-select>
       </a-form-item>
+      <a-form-item
+        label="告警级别"
+        :labelCol="labelCol"
+        :wrapperCol="wrapperCol"
+      >
+        <a-select
+          mode="multiple"
+          allowClear
+          placeholder="请选择告警级别"
+          v-decorator="[
+            'alarmLevel',
+            { rules: [{ required: true, message: '请选择告警级别' }] },
+          ]"
+          @change="alarmLevelChange"
+        >
+          <a-select-option value="checkall" key="checkall">全选</a-select-option>
+          <a-select-option
+            v-for="item in levelList"
+            :key="item"
+            :value="item"
+          >
+            {{ item }}
+          </a-select-option>
+        </a-select>
+      </a-form-item>
       <a-form-item :wrapperCol="{span: 19, offset: 5}">
-        <a-button style="margin-left: 8px" @click="prevStep">上一步</a-button>
-        <a-button type="primary" @click="nextStep">下一步</a-button>
+        <a-button @click="prevStep">上一步</a-button>
+        <a-button style="margin-left: 8px" type="primary" @click="nextStep">下一步</a-button>
       </a-form-item>
     </a-form>
   </div>
@@ -96,7 +121,7 @@
 <script>
 import screening from '@/views/alarm/screening'
 export default {
-  name: 'Step2',
+  name: 'Rules',
   data () {
     return {
       screening,
@@ -104,28 +129,51 @@ export default {
       wrapperCol: { lg: { span: 19 }, sm: { span: 19 } },
       form: this.$form.createForm(this),
       loading: false,
-      timer: 0
+      timer: 0,
+      levelList: [
+        'INFO', 'WARNING', 'MINOR', 'MAJOR', 'CRITICAL'
+      ]
     }
   },
   methods: {
     nextStep () {
-      const that = this
       const { form: { validateFields } } = this
-      that.loading = true
+      // 先校验，通过表单校验后，才进入下一步
       validateFields((err, values) => {
         if (!err) {
-          console.log('表单 values', values)
-          that.timer = setTimeout(function () {
-            that.loading = false
-            that.$emit('nextStep')
-          }, 1500)
-        } else {
-          that.loading = false
+          this.$emit('nextStep')
         }
       })
     },
     prevStep () {
       this.$emit('prevStep')
+    },
+    /**
+     * 告警类型改变
+     */
+    alarmLevelChange (value) {
+      // this.queryParam.alarmType = screening.checkAll(value, this.alarmType)
+      console.log(value)
+      let list = value
+      value.forEach(element => {
+        if (element === 'checkall') {
+          if (length - 1 === this.levelList && value[length - 1] === 'checkall') {
+            list = []
+            console.log('点击')
+          } else {
+            list = []
+            this.levelList.forEach(m => {
+              list.push(m)
+              console.log(list)
+              const a = this.form.getFieldsValue()
+              this.form.setFieldsValue({
+                alarmLevel: list
+              })
+              console.log(a)
+            })
+          }
+        }
+      })
     }
   },
   beforeDestroy () {
