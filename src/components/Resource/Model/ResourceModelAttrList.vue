83345827d7c2e796f4e435d5193ae47ef220515d
tanghongxin <hongxin.tang@hotmail.com>
2020-02-17 10:07:15 +0800

    :bug: (资源模型): 属性列表查询功能缺失

--- src/components/Resource/Model/ResourceModelAttrList.vue
+++ src/components/Resource/Model/ResourceModelAttrList.vue
@@ -8,12 +8,50 @@
       :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: selectRow}"
       :scroll="{ x: 1580, y: 850}"
     >
+      <template #query>
+        <a-form layout="inline">
+          <div :class="{ fold: !advanced }">
+            <a-row>
+              <a-col :md="12" :sm="24">
+                <a-form-item
+                  label="属性名称"
+                  :labelCol="{ span: 4 }"
+                  :wrapperCol="{ span: 14, offset: 2 }"
+                  style="width: 100%"
+                >
+                  <a-input v-model="queryParams.name_s" placeholder=""/>
+                </a-form-item>
+              </a-col>
+              <a-col :md="12" :sm="24">
+                <a-form-item
+                  label="显示名称"
+                  :labelCol="{ span: 4 }"
+                  :wrapperCol="{ span: 14, offset: 2 }"
+                  style="width: 100%"
+                >
+                  <a-input v-model="queryParams.label_s" placeholder=""/>
+                </a-form-item>
+              </a-col>
+            </a-row>
+          </div>
+
+          <!-- TODO: 统一管理布局 -->
+          <!-- TODO: 居中 span -->
+          <span :style=" { float: 'right', overflow: 'hidden', transform: `translateY(${!advanced ? '6.5' : '15.5'}px)` } || {} ">
+            <a-button type="primary" @click="query">查询</a-button>
+            <a-button style="margin-left: 8px" @click="queryParams = {}">重置</a-button>
+            <!--            <a @click="toggleAdvanced" style="margin-left: 8px">-->
+            <!--              {{ advanced ? '收起' : '展开' }}-->
+            <!--              <a-icon :type="advanced ? 'up' : 'down'"/>-->
+            <!--            </a>-->
+          </span>
+        </a-form>
+      </template>
       <template #operation>
         <a-button @click="add">新建</a-button>
         <a-button @click="edit" :disabled="selectedRowKeys.length !== 1">编辑</a-button>
         <a-button @click="batchDelete" :disabled="selectedRowKeys.length === 0">删除</a-button>
         <a-button>数据检查</a-button>
-        <a-button>筛选</a-button>
       </template>
     </CTable>
 
@@ -32,6 +70,7 @@ import gql from 'graphql-tag'
 import apollo from '@/utils/apollo'
 import ResourceModelAttrSchema from './ResourceModelAttrSchema'
 import deleteCheck from '@/components/DeleteCheck'
+import Template from '../../../views/design/moduels/template/index'
 
 const query = gql`query ($where:ngecc_model_attributes_bool_exp = {}, $limit: Int! = 50, $offset: Int! = 0, $orderBy: [ngecc_model_attributes_order_by!]) {
   pagination: ngecc_model_attributes_aggregate (where: $where) {
@@ -71,6 +110,7 @@ const query = gql`query ($where:ngecc_model_attributes_bool_exp = {}, $limit: In
 export default {
   name: 'ResourceModelAttrList',
   components: {
+    Template,
     CTable,
     ResourceModelAttrSchema
   },
@@ -81,6 +121,7 @@ export default {
     }
   },
   data: () => ({
+    advanced: false,
     // 查询参数
     queryParams: {},
     // 选中行
@@ -191,12 +232,23 @@ export default {
           },
           ...parameter,
           where: {
-            ...this.where
+            ...this.where,
+            ...this.queryParams.label_s ? {
+              label_s: {
+                _ilike: `%${this.queryParams.label_s.trim()}%`
+              }
+            } : {},
+            ...this.queryParams.name_s ? {
+              name_s: {
+                _ilike: `%${this.queryParams.name_s.trim()}%`
+              }
+            } : {}
           }
         }
       }).then(r => r.data)
     },
     query () {
+      console.log(1)
       this.$refs['table'].refresh(true)
     },
     /**
@@ -219,5 +271,8 @@ export default {
 </script>
 
 <style lang="less">
-
+.fold {
+  display: inline-block;
+  width: calc(100% - 216px);
+}
 </style>
