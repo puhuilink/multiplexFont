dfa4d54be45f98a9e7f6de29803423c0aba6b2da
lyz <lyz02413@163.com>
2020-03-04 00:50:27 +0800

    新增x2js依赖以及xml&json互转

--- src/views/alarm/screening.js
+++ src/views/alarm/screening.js
@@ -1,187 +1,3 @@
-// 筛选项：CI域
-const CIDomain = [
-  {
-    label: 'Root',
-    options: [{
-      value: 'rootDamin',
-      label: 'rootDamin'
-    }]
-  }, {
-    label: 'rootDamin',
-    options: [{
-      value: 'bj',
-      label: '北京运维组'
-    },
-    {
-      value: 'xm',
-      label: '厦门运维组'
-    }]
-  }
-]
-
-// 筛选项：CI类型
-const CIType = [
-  {
-    label: 'Root',
-    options: [{
-      value: '0',
-      label: '统一资源'
-    }]
-  },
-  {
-    label: 'CI',
-    options: [{
-      value: '1',
-      label: '管理'
-    },
-    {
-      value: '2',
-      label: '监控定义'
-    },
-    {
-      value: '3',
-      label: '监控对象'
-    },
-    {
-      value: '4',
-      label: '告警'
-    },
-    {
-      value: '5',
-      label: '性能'
-    }]
-  },
-  {
-    label: 'Manager',
-    options: [{
-      value: '6',
-      label: '字典数据'
-    }]
-  }
-]
-
-// 筛选项 CI实例
-const CIInstance = [
-  {
-    label: 'Root',
-    options: [{
-      value: '1',
-      label: '统一资源'
-    }]
-  },
-  {
-    label: 'CI',
-    options: [{
-      value: '2',
-      label: '管理'
-    },
-    {
-      value: '3',
-      label: '监控定义'
-    },
-    {
-      value: '4',
-      label: '监控对象'
-    },
-    {
-      value: '5',
-      label: '告警'
-    },
-    {
-      value: '6',
-      label: '性能'
-    }]
-  },
-  {
-    label: 'Manager',
-    options: [{
-      value: '7',
-      label: '字典数据'
-    }]
-  }
-]
-
-// 筛选项: CI名称
-const CIName = [
-  {
-    label: 'Root',
-    options: [{
-      value: '1',
-      label: '统一资源'
-    }]
-  },
-  {
-    label: 'CI',
-    options: [{
-      value: '2',
-      label: '管理'
-    },
-    {
-      value: '3',
-      label: '监控定义'
-    },
-    {
-      value: '4',
-      label: '监控对象'
-    },
-    {
-      value: '5',
-      label: '告警'
-    },
-    {
-      value: '6',
-      label: '性能'
-    }]
-  },
-  {
-    label: 'Manager',
-    options: [{
-      value: '7',
-      label: '字典数据'
-    }]
-  }
-]
-
-// 告警类型
-const alarmType = [
-  {
-    label: 'Root',
-    options: [{
-      value: '1',
-      label: '统一资源'
-    }]
-  },
-  {
-    label: 'CI',
-    options: [{
-      value: '2',
-      label: '管理'
-    },
-    {
-      value: '3',
-      label: '监控定义'
-    },
-    {
-      value: '4',
-      label: '监控对象'
-    },
-    {
-      value: '5',
-      label: '告警'
-    },
-    {
-      value: '6',
-      label: '性能'
-    }]
-  },
-  {
-    label: 'Manager',
-    options: [{
-      value: '7',
-      label: '字典数据'
-    }]
-  }
-]
 
 // 级别列表
 const levelList = {
@@ -221,52 +37,6 @@ const forwardType = [
     label: '短信'
   }
 ]
-
-/**
- * 获得今天的日期 yyyy-mm-dd
- */
-function getNowFormatDate () {
-  var date = new Date()
-  var seperator1 = '-'
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var strDate = date.getDate()
-  if (month >= 1 && month <= 9) {
-    month = '0' + month
-  }
-  if (strDate >= 0 && strDate <= 9) {
-    strDate = '0' + strDate
-  }
-  var currentdate = year + seperator1 + month + seperator1 + strDate
-  return currentdate
-}
-
-/**
- * 选择框全选(适用于分组选择器)
- */
-function checkAll (arr, modelList) {
-  // arr:change中的数组 ，  modelList:下拉框List
-  const length = arr.length
-  let list = arr
-  arr.forEach(element => {
-    // 当数组中存在checkall，说明此时进行全选/取消全选
-    if (element === 'checkall') {
-      // 当数组长度为最大长度且最后一个元素为checkall时，说明此时在全选的基础上又点击全选，则取消全选
-      if (length - 1 === modelList.length && arr[length - 1] === 'checkall') {
-        list = []
-      } else {
-        // 当不是取消全选操作，只要数组中出现了checkall则说明进行了全选操作
-        list = []
-        modelList.forEach(m => {
-          for (const i in m) {
-            list.push(m[i].name_s)
-          }
-        })
-      }
-    }
-  })
-  return list
-}
 
 // 告警状态列表
 const stateList = [
@@ -319,17 +89,73 @@ const severityList = [
     text: 'CRITICAL'
   }
 ]
+/**
+ * 获得今天的日期 yyyy-mm-dd
+ */
+function getNowFormatDate () {
+  var date = new Date()
+  var seperator1 = '-'
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var strDate = date.getDate()
+  if (month >= 1 && month <= 9) {
+    month = '0' + month
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = '0' + strDate
+  }
+  var currentdate = year + seperator1 + month + seperator1 + strDate
+  return currentdate
+}
+
+/**
+ * 选择框全选(适用于分组选择器)
+ */
+function checkAll (arr, modelList) {
+  // arr:change中的数组 ，  modelList:下拉框List
+  const length = arr.length
+  let list = arr
+  arr.forEach(element => {
+    // 当数组中存在checkall，说明此时进行全选/取消全选
+    if (element === 'checkall') {
+      // 当数组长度为最大长度且最后一个元素为checkall时，说明此时在全选的基础上又点击全选，则取消全选
+      if (length - 1 === modelList.length && arr[length - 1] === 'checkall') {
+        list = []
+      } else {
+        // 当不是取消全选操作，只要数组中出现了checkall则说明进行了全选操作
+        list = []
+        modelList.forEach(m => {
+          for (const i in m) {
+            list.push(m[i].name_s)
+          }
+        })
+      }
+    }
+  })
+  return list
+}
+
+function xmlTojson (xml) {
+  const X2JS = require('x2js')
+  var x2js = new X2JS()
+  return x2js.xml2js(xml)
+}
+/**
+ * xml数据转成json对象的数据
+ */
+function jsonToxml (json) {
+  const X2JS = require('x2js')
+  var x2js = new X2JS()
+  return x2js.js2xml(json)
+}
 
 export default {
-  CIDomain,
-  CIType,
-  CIInstance,
-  CIName,
-  alarmType,
   levelList,
   forwardType,
   getNowFormatDate,
   checkAll,
   stateList,
-  severityList
+  severityList,
+  xmlTojson,
+  jsonToxml
 }
