e36be82f544f9af1630a46f2deb2a1bef6dd6aeb
tanghongxin <hongxin.tang@hotmail.com>
2020-03-15 17:40:24 +0800

    :zap: (资源) 为新旧api字段增加mapping关系

--- src/api/controller/Resource.js
+++ src/api/controller/Resource.js
@@ -8,6 +8,7 @@ import {
   queryKpiList
 } from '../graphql/Resource'
 import { oldRequest } from '@/utils/oldRequest'
+import { modelMapping } from '../mapping/Resource'
 
 export const getResourceInstanceList = function () {
   return apollo.clients.resource.query({
@@ -53,38 +54,37 @@ export const getKpiList = function (where = {}) {
  * @param {Objetc} set 增量内容
  */
 export const editModel = function (did, set = {}) {
-  return apollo.clients.resource.mutate({
-    mutation: mutationUpdateModel,
-    variables: {
-      did,
-      set
-    }
-  })
+  // return apollo.clients.resource.mutate({
+  //   mutation: mutationUpdateModel,
+  //   variables: {
+  //     did,
+  //     set
+  //   }
+  // })
+  return editModelOld(did, set)
 }
 
 /**
  * 旧系统更新模型
  */
 export const editModelOld = function (did, set = {}) {
-  const {
-    name_s: name,
-    label_s: label,
-    icon_s: icon,
-    edit_b: edit,
-    batch_b: batch,
-    encrypt_s: encrypt,
-    order_i: order
-  } = set
+  const data = {}
+  Object.keys(set).forEach(key => {
+    if (modelMapping[key]) {
+      data[modelMapping[key]] = set[key]
+    }
+  })
+  // const {
+  //   name_s: name,
+  //   label_s: label,
+  //   icon_s: icon,
+  //   edit_b: edit,
+  //   batch_b: batch,
+  //   encrypt_s: encrypt,
+  //   order_i: order
+  // } = set
 
-  return oldRequest.post('/urmp/api/rest/post/modelService/update', [{
-    name,
-    label,
-    icon,
-    edit,
-    batch,
-    encrypt,
-    order
-  }, '', []])
+  return oldRequest.post('/urmp/api/rest/post/modelService/update', [data, '', []])
 }
 
 /**
@@ -93,6 +93,23 @@ export const editModelOld = function (did, set = {}) {
  * @return {*}
  */
 export const addModels = function (objects = []) {
+  // TODO: 数据表 did 唯一，是否要（需要）做 name_s 唯一？
+  // TODO: 旧的业务逻辑，确实是根据 name_s 唯一的，如何迁移到以 did 构建树？
+  // FIXME: 目前的构建树方式，是认为 name_s 唯一的
+  return apollo.clients.resource.mutate({
+    mutation: mutationInsertModels,
+    variables: {
+      objects
+    }
+  })
+}
+
+/**
+ * 旧系统（批量）新增资源模型
+ * @param {Array} objects
+ * @return {*}
+ */
+export const addModelsOld = function (objects = []) {
   // TODO: 数据表 did 唯一，是否要（需要）做 name_s 唯一？
   // TODO: 旧的业务逻辑，确实是根据 name_s 唯一的，如何迁移到以 did 构建树？
   // FIXME: 目前的构建树方式，是认为 name_s 唯一的
---
+++ src/api/mapping/Resource.js
@@ -0,0 +1,12 @@
+// 新旧系统数据表字段映射关系
+
+export const modelMapping = {
+  name_s: 'name',
+  label_s: 'label',
+  icon_s: 'icon',
+  edit_b: 'edit',
+  batch_b: 'batch',
+  encrypt_s: 'encrypt',
+  order_i: 'order',
+  parentname_s: 'parentName'
+}
