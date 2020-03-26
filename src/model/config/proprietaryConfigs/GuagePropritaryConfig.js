dd46273e0f390630b48db951c07cb09db418ec92
tanghongxin <hongxin.tang@hotmail.com>
2020-03-26 15:17:00 +0800

    :sparkles: (视图组件) 初始化仪表盘组件

--- src/model/config/proprietaryConfigs/GuagePropritaryConfig.js
+++ src/model/config/proprietaryConfigs/GuagePropritaryConfig.js
@@ -6,8 +6,74 @@ import { SeriesGauge } from './index'
  */
 export default class GuageProprietaryConfig {
   constructor ({
-    series = {},
-    backgroundColor = '#ddd'
+    series = {
+      name: '速度',
+      type: 'gauge',
+      min: 0,
+      max: 220,
+      splitNumber: 11,
+      radius: '80%',
+      axisLine: { // 坐标轴线
+        lineStyle: { // 属性lineStyle控制线条样式
+          color: [[0.09, 'lime'], [0.82, '#1e90ff'], [1, '#ff4500']],
+          width: 3,
+          shadowColor: '#fff', // 默认透明
+          shadowBlur: 10
+        }
+      },
+      axisLabel: { // 坐标轴小标记
+        fontWeight: 'bolder',
+        color: '#fff',
+        shadowColor: '#fff', // 默认透明
+        shadowBlur: 10
+      },
+      axisTick: { // 坐标轴小标记
+        length: 15, // 属性length控制线长
+        lineStyle: { // 属性lineStyle控制线条样式
+          color: 'auto',
+          shadowColor: '#fff', // 默认透明
+          shadowBlur: 10
+        }
+      },
+      splitLine: { // 分隔线
+        length: 25, // 属性length控制线长
+        lineStyle: { // 属性lineStyle（详见lineStyle）控制线条样式
+          width: 3,
+          color: '#fff',
+          shadowColor: '#fff', // 默认透明
+          shadowBlur: 10
+        }
+      },
+      pointer: { // 分隔线
+        shadowColor: '#fff', // 默认透明
+        shadowBlur: 5
+      },
+      title: {
+        textStyle: { // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+          fontWeight: 'bolder',
+          fontSize: 20,
+          fontStyle: 'italic',
+          color: '#fff',
+          shadowColor: '#fff', // 默认透明
+          shadowBlur: 10,
+          text: 'text'
+        }
+      },
+      detail: {
+        backgroundColor: 'rgba(30,144,255,0.8)',
+        borderWidth: 1,
+        borderColor: '#fff',
+        shadowColor: '#fff', // 默认透明
+        shadowBlur: 5,
+        offsetCenter: [0, '50%'], // x, y，单位px
+        textStyle: { // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+          fontWeight: 'bolder',
+          color: '#fff'
+        }
+      },
+      data: [{ value: 0, name: 'km/h' }]
+    },
+    backgroundColor = '#1b1b1b'
   }) {
     this.series = new SeriesGauge(series)
     this.backgroundColor = backgroundColor
--- src/views/design/modules/config/charts/Guage.vue
+++ src/views/design/modules/config/charts/Guage.vue
@@ -1,41 +1,135 @@
 <template>
   <div class="texts-config">
-    <a-tabs
-      defaultActiveKey="1"
-      tabPosition="top"
-      :style="{ height: '100%'}"
-    >
+    <a-tabs defaultActiveKey="1" tabPosition="top" :style="{ height: '100%'}">
       <a-tab-pane tab="公共属性" key="1">
-
         <!-- S 公共配置模板 -->
         <CommonTemplate />
         <!-- E 公共配置模板 -->
-
       </a-tab-pane>
 
       <a-tab-pane tab="专有属性" key="2">
         <div class="texts-config__template">
           <a-collapse defaultActiveKey="1" :bordered="false">
-
             <!-- S 文本样式 -->
             <a-collapse-panel header="文本样式" key="1">
-
               <div class="comment-template__item">
-                <p class="comment-template__leading">颜色:</p>
+                <p class="comment-template__leading">最小值:</p>
                 <div class="comment-template__inner">
-                  <ColorPicker
-                    v-model="config.proprietaryConfig.series.axisTick.lineStyle.color"
-                    @change="change()" />
+                  <a-input
+                    type="number"
+                    v-model="config.proprietaryConfig.series.min"
+                    @change="change()"
+                  />
                 </div>
               </div>
 
+              <div class="comment-template__item">
+                <p class="comment-template__leading">最大值:</p>
+                <div class="comment-template__inner">
+                  <a-input
+                    type="number"
+                    v-model="config.proprietaryConfig.series.max"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">仪表半径:</p>
+                <div class="comment-template__inner">
+                  <a-slider
+                    :min="10"
+                    :max="90"
+                    tooltipVisible
+                    :value="~~config.proprietaryConfig.series.radius.replace('%', '')"
+                    @change="radiusChange"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">刻度区间:</p>
+                <div class="comment-template__inner">
+                  <a-input
+                    type="number"
+                    v-model="config.proprietaryConfig.series.max"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">刻度线长度:</p>
+                <div class="comment-template__inner">
+                  <a-input
+                    type="number"
+                    v-model="config.proprietaryConfig.series.axisTick.length"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">刻度线颜色:</p>
+                <div class="comment-template__inner">
+                  <ColorPicker
+                    v-model="config.proprietaryConfig.series.axisTick.lineStyle.color"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">分割线长度:</p>
+                <div class="comment-template__inner">
+                  <a-input
+                    type="number"
+                    v-model="config.proprietaryConfig.series.splitLine.length"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">分隔线颜色:</p>
+                <div class="comment-template__inner">
+                  <ColorPicker
+                    v-model="config.proprietaryConfig.series.splitLine.lineStyle.color"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">分割区间长度:</p>
+                <div class="comment-template__inner">
+                  <a-input
+                    type="number"
+                    v-model="config.proprietaryConfig.series.splitNumber"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+
+              <div class="comment-template__item">
+                <p class="comment-template__leading">标题文字:</p>
+                <div class="comment-template__inner">
+                  <a-input
+                    v-model="config.proprietaryConfig.series.data[0].name"
+                    @change="change()"
+                  />
+                </div>
+              </div>
+              <!--
+              数值名称
+              刻度线颜色
+              分割线颜色
+              -->
             </a-collapse-panel>
             <!-- E 文本样式 -->
-
           </a-collapse>
         </div>
       </a-tab-pane>
-
     </a-tabs>
   </div>
 </template>
@@ -54,37 +148,8 @@ export default {
     ColorPicker
   },
   methods: {
-    positionChange () {
-      const { position } = this.config.proprietaryConfig.title
-      switch (position.mode) {
-        case 'center':
-          Object.assign(position, { editablePosition: [] })
-          break
-        case 'center_left':
-          Object.assign(position, { editablePosition: ['left'], left: 0 })
-          break
-        case 'center_right':
-          Object.assign(position, { editablePosition: ['right'], right: 0 })
-          break
-        case 'top_center':
-          Object.assign(position, { editablePosition: ['top'], top: 0 })
-          break
-        case 'bottom_center':
-          Object.assign(position, { editablePosition: ['bottom'], bottom: 0 })
-          break
-        case 'custom':
-          Object.assign(position, {
-            editablePosition: ['top', 'bottom', 'left', 'right'],
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0
-          })
-          break
-        default:
-          Object.assign(position, { editablePosition: [] })
-          break
-      }
+    radiusChange (value) {
+      this.config.proprietaryConfig.series.radius = `${value}%`
       this.change()
     }
   }
