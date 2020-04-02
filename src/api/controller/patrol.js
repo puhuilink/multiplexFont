cc2a2ca3a32c1f0f33aa16aff000c98c9f97735e
lyz <lyz02413@163.com>
2020-04-02 09:45:01 +0800

    ➕巡检任务单初版（详情有待完善）

--- src/api/controller/patrol.js
+++ src/api/controller/patrol.js
@@ -1,9 +1,15 @@
 import apollo from '@/utils/apollo'
 import {
   queryRouteList,
-  queryPlanList
+  queryPlanList,
+  queryUserGroupList,
+  queryTaskInfo,
+  updatePlan,
+  deleteRouteArr,
+  deletePlanArr
 } from '../graphql/patrol'
 
+// get
 export const getRouteList = function (variables = {}) {
   return apollo.clients.patrol.query({
     query: queryRouteList,
@@ -16,6 +22,51 @@ export const getRouteList = function (variables = {}) {
 export const getPlanList = function (variables = {}) {
   return apollo.clients.patrol.query({
     query: queryPlanList,
+    variables: {
+      ...variables
+    }
+  })
+}
+
+export const getUserGroupList = function (variables = {}) {
+  return apollo.clients.patrol.query({
+    query: queryUserGroupList,
+    variables: {
+      ...variables
+    }
+  })
+}
+
+export const getTaskInfoList = function (variables = {}) {
+  return apollo.clients.patrol.query({
+    query: queryTaskInfo,
+    variables: {
+      ...variables
+    }
+  })
+}
+// mutation
+export const mutatePlan = function (variables = {}) {
+  return apollo.clients.patrol.mutate({
+    mutation: updatePlan,
+    variables: {
+      ...variables
+    }
+  })
+}
+// delete
+export const deleteRoute = function (variables = {}) {
+  return apollo.clients.patrol.mutate({
+    mutation: deleteRouteArr,
+    variables: {
+      ...variables
+    }
+  })
+}
+
+export const deletePlan = function (variables = {}) {
+  return apollo.clients.patrol.mutate({
+    mutation: deletePlanArr,
     variables: {
       ...variables
     }
--- src/api/graphql/patrol.js
+++ src/api/graphql/patrol.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag'
 
+// query
 export const queryRouteList = gql`query ($where: route_info_bool_exp = {}, $limit: Int! = 0, $offset: Int! = 10,  $orderBy: [route_info_order_by!]) {
   pagination: route_info_aggregate(where: $where) {
     aggregate {
@@ -48,13 +49,8 @@ export const queryPlanList = gql`query ($where: plan_info_bool_exp = {}, $limit:
     }
 }`
 
-export const queryUserGroupList = gql`query ($where: user_group_info_bool_exp = {}, $limit: Int! = 0, $offset: Int! = 10,  $orderBy: [user_group_info_order_by!]) {
-  pagination: user_group_info_aggregate(where: $where) {
-    aggregate {
-      count
-    }
-  }
-  data:  user_group_info (where: $where, offset: $offset, limit: $limit, order_by: $orderBy) {
+export const queryUserGroupList = gql`query{
+  data:  user_group_info {
     update_time
     is_enable
     group_name
@@ -64,4 +60,88 @@ export const queryUserGroupList = gql`query ($where: user_group_info_bool_exp = 
     create_time
     ascription
     }
+}`
+
+export const queryTaskInfo = gql`query ($where: task_info_bool_exp = {}, $limit: Int! = 0, $offset: Int! = 10,  $orderBy: [task_info_order_by!]) {
+  pagination: task_info_aggregate(where: $where) {
+    aggregate {
+      count
+    }
+  }
+  data:  task_info (where: $where, offset: $offset, limit: $limit, order_by: $orderBy) {
+    ascription
+    create_time
+    df_group_code
+    df_transactor_code
+    df_transactor_name
+    group_code
+    is_delay
+    is_df_transactor
+    is_enable
+    plan_code
+    plan_end_time
+    plan_start_time
+    real_end_time
+    real_start_time
+    real_sum_time
+    route_code
+    task_can_start_time
+    task_code
+    task_delay_time
+    task_fail_time
+    task_id
+    task_late_start_time
+    task_name
+    task_state
+    task_status
+    task_type
+    transactor_code
+    transactor_name
+    update_time
+    }
+}`
+
+// mutation
+
+export const updateRoute = gql`mutation update ($where: route_info_bool_exp!, $val: route_info_set_input) {
+  update_route_info  (
+    where: $where,
+    _set: $val
+  ) {
+      returning {
+        route_id
+    }
+  }
+}`
+
+export const updatePlan = gql`mutation update ($where: plan_info_bool_exp!, $val: plan_info_set_input) {
+  update_plan_info  (
+    where: $where,
+    _set: $val
+  ) {
+      returning {
+      plan_id
+    }
+  }
+}`
+
+// delete
+export const deleteRouteArr = gql`mutation ($IDs: [Int!] = []) {
+  delete_route_info (where: {
+    route_id: {
+      _in: $IDs
+    }
+  }) {
+    affected_rows
+  }
+}`
+
+export const deletePlanArr = gql`mutation ($IDs: [Int!] = []) {
+  delete_plan_info (where: {
+    plan_id: {
+      _in: $IDs
+    }
+  }) {
+    affected_rows
+  }
 }`
