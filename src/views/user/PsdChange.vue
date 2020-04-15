e76c60a6f913bbfced7355760d1cbfbba6a48679
tanghongxin <hongxin.tang@hotmail.com>
2020-04-15 18:39:42 +0800

    :sparkles: (重置密码) 对接接口

--- src/views/user/PsdChange.vue
+++ src/views/user/PsdChange.vue
@@ -8,7 +8,7 @@
           type="password"
           autocomplete="false"
           placeholder="原始密码"
-          v-decorator="['oldPsd', {rules: [{ required: true, message: '请输入原始密码' }], validateTrigger: ['change', 'blur']}]"
+          v-decorator="['encryptedPwd', {rules: [{ required: true, message: '请输入原始密码' }], validateTrigger: ['change', 'blur']}]"
         ></a-input>
       </a-form-item>
 
@@ -34,7 +34,7 @@
             @click="handlePasswordInputClick"
             autocomplete="false"
             placeholder="至少8位密码，区分大小写"
-            v-decorator="['password', {rules: [{ required: true, message: '至少8位密码，区分大小写'}, { validator: this.handlePasswordLevel }], validateTrigger: ['change', 'blur']}]"
+            v-decorator="['newEncryptedPwd', {rules: [{ required: true, message: '至少8位密码，区分大小写'}, { validator: this.handlePasswordLevel }], validateTrigger: ['change', 'blur']}]"
           ></a-input>
         </a-form-item>
       </a-popover>
@@ -90,9 +90,9 @@
             type="primary"
             htmlType="submit"
             class="register-button"
-            :loading="registerBtn"
+            :loading="loading"
             @click.stop.prevent="handleSubmit"
-            :disabled="registerBtn">确定重置
+            :disabled="loading">确定重置
           </a-button>
         </a-col>
         <!-- <router-link class="login" :to="{ name: 'login' }">使用已有账户登录</router-link> -->
@@ -106,7 +106,9 @@
 import { mixinDevice } from '@/utils/mixin.js'
 // eslint-disable-next-line no-unused-vars
 import { getSmsCaptcha } from '@/api/login'
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
+import { resetPwd } from '@/api/controller/User'
+// import Timeout from 'await-timeout'
 
 const levelNames = {
   0: '低',
@@ -152,10 +154,11 @@ export default {
         percent: 10,
         progressColor: '#FF0000'
       },
-      registerBtn: false
+      loading: false
     }
   },
   computed: {
+    ...mapGetters(['userId']),
     passwordLevelClass () {
       return levelClass[this.state.passwordLevel]
     },
@@ -233,20 +236,22 @@ export default {
 
     handleSubmit () {
       // eslint-disable-next-line
-      const { form: { validateFields }, state, $router } = this
-      validateFields({ force: true }, (err, values) => {
+      const { form: { validateFields }, state, $router, userId } = this
+      validateFields({ force: true }, async (err, values) => {
         if (!err) {
           state.passwordLevelChecked = false
-          return this.Logout({}).then(() => {
-            setTimeout(() => {
-              window.location.reload()
-            }, 16)
-          }).catch(err => {
-            this.$message.error({
-              title: '错误',
-              description: err.message
+          this.loading = true
+          try {
+            await resetPwd({
+              ...values,
+              userId
             })
-          })
+            this.requestSuccess()
+          } catch (e) {
+            throw e
+          } finally {
+            this.loading = false
+          }
         }
       })
     },
@@ -289,13 +294,43 @@ export default {
         }
       )
     },
+    requestSuccess () {
+      let secondsToGo = 3
+      const modal = this.$success({
+        footer: null,
+        closable: false,
+        keyboard: false,
+        title: '重置密码成功',
+        content: `${secondsToGo}秒后将重定向到登录页面`
+      })
+      const interval = setInterval(() => {
+        secondsToGo -= 1
+        modal.update({
+          content: `${secondsToGo}秒后将重定向到登录页面`
+        })
+      }, 1000)
+      setTimeout(() => {
+        clearInterval(interval)
+        modal.destroy()
+        this.Logout({}).then(() => {
+          setTimeout(() => {
+            window.location.reload()
+          }, 16)
+        }).catch(err => {
+          this.$message.error({
+            title: '错误',
+            description: err.message
+          })
+        })
+      }, secondsToGo * 1000)
+    },
     requestFailed (err) {
       this.$notification['error']({
         message: '错误',
         description: ((err.response || {}).data || {}).message || '请求出现错误，请稍后再试',
         duration: 4
       })
-      this.registerBtn = false
+      this.loading = false
     }
   },
   watch: {
