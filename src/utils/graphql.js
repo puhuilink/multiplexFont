cab07f735bc3c1ab8981a5e697fa6e36abdfffb2
tanghongxin <hongxin.tang@hotmail.com>
2020-04-24 10:46:53 +0800

    :bug: (审计管理) 时间查询条件不生效

---
+++ src/utils/graphql.js
@@ -0,0 +1,50 @@
+// import _ from 'lodash'
+import moment from 'moment'
+
+const isAvailable = function (v) {
+  if (Array.isArray(v)) {
+    return v.length
+  }
+  if (typeof v === 'string') {
+    return v.trim()
+  }
+  return v !== undefined && v !== null
+}
+
+const generate = function ({ key, value }) {
+  if (Array.isArray(value)) {
+    if (value.length === 2 && value[1] instanceof moment) {
+      return {
+        [key]: {
+          _gt: value[0].format('YYYY-MM-DDTHH:mm:ss'),
+          _lt: value[1].format('YYYY-MM-DDTHH:mm:ss')
+        }
+      }
+    }
+  }
+  if (typeof value === 'string') {
+    return {
+      [key]: {
+        _ilike: `%${value.trim()}%`
+      }
+    }
+  }
+  if (typeof value === 'number') {
+    return {
+      [key]: {
+        _eq: value
+      }
+    }
+  }
+  return {}
+}
+
+export const generateQuery = function (params = {}) {
+  let obj = {}
+  Object
+    .entries(params)
+    .filter(([key, value]) => isAvailable(value))
+    .map(([key, value]) => generate({ key, value }))
+    .forEach((v) => { obj = { ...obj, ...v } })
+  return obj
+}
