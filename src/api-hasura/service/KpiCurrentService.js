111b1edb27ddc9697fa340cadad2f51702744c68
tanghongxin <hongxin.tang@hotmail.com>
2020-04-24 18:40:36 +0800

    :construction: (api-hasura) 指标、资源实例

--- src/api-hasura/service/KpiCurrentService.js
+++ src/api-hasura/service/KpiCurrentService.js
@@ -8,11 +8,33 @@ import { BaseService } from './BaseService'
 import { query } from '../utils/hasura-orm/index'
 
 class KpiCurrentService extends BaseService {
-  static async _getKpiAndCiInfo ({ selectedKpi, selectedInstance }) {
-    await Promise.all([
-      ...selectedKpi.map(kpicode_s => InstanceValuesDao.find({ where: kpicode_s })),
-      ...selectedInstance.map(_id_s => InstanceDao.find({ where: _id_s }))
+  /**
+   * 获取选中的 ci 与 kpi 的基本信息
+   * @param {{Object}} param0
+   * @return {Promise<Array<HasuraORM>>}
+   */
+  static async _getKpiAndCiInfo ({ selectedKpi = [], selectedInstance = [] }) {
+    // 将 ci 与 kpi 一一组合
+    const composedValue = []
+    selectedInstance.forEach(ci_id => {
+      selectedKpi.forEach(kpi_code => {
+        composedValue.push({ kpi_code, ci_id })
+      })
+    })
+
+    // 生成 ci 与 kpi 的基本信息的查询语句
+    const infoQueryList = await Promise.all([
+      ...selectedKpi.map(kpicode_s => InstanceValuesDao.find({ where: { kpicode_s }, fields: ['kpicode_s', 'label_s'] })),
+      ...selectedInstance.map(_id_s => InstanceDao.find({ where: { _id_s }, fields: ['_id_s', 'label_s'] }))
     ])
+
+    // 查询基本信息与指标值
+    await query(
+      ...infoQueryList,
+      ...composedValue.map(({ ci_id, kpi_code }) => KpiCurrentLastestDao.findLastestOne({ where: { ci_id, kpi_code }, fields: ['ci_id', 'kpi_code', 'kpi_value_num'] }))
+    )
+
+    // 组合返回体
   }
   /**
    * 获取一条或多条最新指标
