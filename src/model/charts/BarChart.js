22c5400c88612607640839c199d7335b46ed5c63
tanghongxin <hongxin.tang@hotmail.com>
2020-04-26 11:10:16 +0800

    :art: （视图定制）柱形图静态数据隐藏不必要的配置

--- src/model/charts/BarChart.js
+++ src/model/charts/BarChart.js
@@ -79,12 +79,13 @@ export default class BarChart extends Chart {
       }
     }
     return Object.assign({}, option, {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: { // 坐标轴指示器，坐标轴触发有效
-          type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-        }
-      }
+      // TODO: ci 数据未完全展示
+      // tooltip: {
+      //   trigger: 'axis',
+      //   axisPointer: { // 坐标轴指示器，坐标轴触发有效
+      //     type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+      //   }
+      // }
     })
   }
 }
