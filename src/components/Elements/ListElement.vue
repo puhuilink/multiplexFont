f7db4f2473352062c9c6a69b31ec5366c2533502
tanghongxin <hongxin.tang@hotmail.com>
2020-04-29 11:21:05 +0800

    :sparkles: (视图定制) 列表支持分页查询

--- src/components/Elements/ListElement.vue
+++ src/components/Elements/ListElement.vue
@@ -3,11 +3,13 @@
 
     <!-- S 列表 -->
     <CTable
+      rowKey="arising_time"
       ref="table"
       :columns="columns"
       :data="loadData"
       :alert="false"
-      :scroll="{ x: 1800, y: 350 }"
+      :scroll="{ x: scrollX, y: 350 }"
+      :pagination="pagination"
     >
     </CTable>
     <!-- E 列表 -->
@@ -15,30 +17,9 @@
 </template>
 
 <script>
-import gql from 'graphql-tag'
-import apollo from '@/utils/apollo'
 import CTable from '@/components/Table/CTable'
-
-const query = gql`query instanceList($where: t_kpi_current_bool_exp! = {}, $limit: Int! = 0, $offset: Int! = 10, $orderBy: [t_kpi_current_order_by!]) {
-  pagination: t_kpi_current_aggregate(where: $where) {
-    aggregate {
-      count
-    }
-  }
-  data: t_kpi_current(offset: $offset, limit: $limit, where: $where, order_by: $orderBy) {
-    agent_id
-    arising_index
-    arising_time
-    ci_id
-    insert_time
-    instance_id
-    kpi_code
-    kpi_value_num
-    kpi_value_txt
-    notes
-    task_id
-  }
-}`
+import { KpiCurrentService } from '@/api-hasura'
+import { TimeRange } from '@/model/config/dataConfig/dynamicData/index'
 
 export default {
   name: 'ListElement',
@@ -60,20 +41,26 @@ export default {
         {
           title: '时间',
           dataIndex: 'arising_time',
-          width: 75,
-          fixed: 'left',
+          width: 150,
+          // fixed: 'left',
           sorter: true
         },
         {
-          title: '名称',
-          dataIndex: 'state',
-          width: 75,
-          sorter: true
+          title: 'ci',
+          // 该字段非数据表字段，为接口返回拼接字段
+          dataIndex: 'instanceLabel',
+          width: 200
         },
         {
-          title: 'kpi值',
+          title: 'kpi',
+          // 该字段非数据表字段，为接口返回拼接字段
+          dataIndex: 'kpiLabel',
+          width: 150
+        },
+        {
+          title: '值',
           dataIndex: 'kpi_value_num',
-          width: 200,
+          width: 100,
           sorter: true
         }
       ],
@@ -81,32 +68,44 @@ export default {
       timer: null
     }
   },
-  created () {},
+  computed: {
+    scrollX () {
+      return this.columns.map(column => column.width || 60).reduce((x1, x2) => x1 + x2)
+    },
+    pagination () {
+      return {
+        // 受限于组件的尺寸，提供相对符合展示效果的分页
+        // FIXME: 选择框定位溢出
+        pageSizeOptions: ['3', '10', '20', '50', '100'],
+        pageSize: 10,
+        defaultPageSize: 10
+      }
+    }
+  },
   methods: {
     /**
      * 加载表格数据
      * @param {Object} parameter CTable 回传的分页与排序条件
      */
     loadData (parameter) {
-      return apollo.clients.alert.query({
-        query,
-        variables: {
-          ...parameter,
-          where: {
-            ...this.where,
-            ...this.elementProps.params.resourceConfig ? {
-              ci_id: {
-                _in: this.elementProps.params.resourceConfig.selectedInstance
-              }
-            } : {},
-            ...this.elementProps.params.resourceConfig ? {
-              kpi_code: {
-                _in: this.elementProps.params.resourceConfig.selectedKpi
-              }
-            } : {}
-          }
-        }
-      }).then(r => r.data)
+      const { resourceConfig, timeRange } = this.elementProps.params
+      if (resourceConfig) {
+        const {
+          selectedInstance,
+          selectedKpi
+        } = resourceConfig
+        return KpiCurrentService.getValue({
+          selectedInstance,
+          selectedKpi,
+          // 此时拿到的可能是未经实例化的 timeRange 对象
+          timeRange: TimeRange.getOption.apply(timeRange),
+          fields: [
+            'kpi_value_num',
+            'arising_time'
+          ],
+          ...parameter
+        })
+      }
     },
     query () {
       this.$refs['table'].refresh(true)
