998ae638b219ac5cc5a162c01d21be1d0d8ea23f
lyz <lyz02413@163.com>
2020-05-20 17:57:30 +0800

    ➖ 去除X2JS

--- src/views/alarm/screening.js
+++ src/views/alarm/screening.js
@@ -1,5 +1,3 @@
-const X2JS = require('x2js')
-const moment = require('moment')
 // 级别列表
 const levelList = {
   0: {
@@ -110,57 +108,6 @@ function getNowFormatDate () {
 }
 
 /**
- * 选择框全选(适用于分组选择器)
- */
-function checkAll (arr, modelList) {
-  // arr:change中的数组 ，  modelList:下拉框List
-  const length = arr.length
-  let list = arr
-  arr.forEach(element => {
-    // 当数组中存在checkall，说明此时进行全选/取消全选
-    if (element === 'checkall') {
-      // 当数组长度为最大长度且最后一个元素为checkall时，说明此时在全选的基础上又点击全选，则取消全选
-      if (length - 1 === modelList.length && arr[length - 1] === 'checkall') {
-        list = []
-      } else {
-        // 当不是取消全选操作，只要数组中出现了checkall则说明进行了全选操作
-        list = []
-        modelList.forEach(m => {
-          for (const i in m) {
-            list.push(m[i].name_s)
-          }
-        })
-      }
-    }
-  })
-  return list
-}
-
-function xmlTojson (xml) {
-  var x2js = new X2JS()
-  return x2js.xml2js(xml)
-}
-/**
- * xml数据转成json对象的数据
- */
-function jsonToxml (json) {
-  var x2js = new X2JS()
-  return x2js.js2xml(json)
-}
-
-/**
- * 时间戳转成日期
- */
-function timeToDate (time) {
-  return moment(time).format('YYYY-MM-DD HH:mm:ss')
-}
-/**
- * 日期转为时间戳
- */
-function dateToTime (date) {
-  return moment(date, 'YYYY-MM-DD HH:mm:ss').valueOf()
-}
-/**
  * 生成唯一id
  */
 function createUniqueId () {
@@ -183,12 +130,7 @@ export default {
   levelList,
   forwardType,
   getNowFormatDate,
-  checkAll,
   stateList,
   severityList,
-  xmlTojson,
-  jsonToxml,
-  timeToDate,
-  dateToTime,
   createUniqueId
 }
