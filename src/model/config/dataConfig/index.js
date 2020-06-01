2e482f0b3ff462ddc8c2cfd37c9765d55c836eb5
tanghongxin <hongxin.tang@hotmail.com>
2020-06-01 18:11:47 +0800

    🚧 (视图设计) 阈值变色

--- src/model/config/dataConfig/index.js
+++ src/model/config/dataConfig/index.js
@@ -22,3 +22,100 @@ export default class DataConfig {
     this.dbDataConfig = CreateDynamicDataFactory.create(widgetType, dbDataConfig)
   }
 }
+
+/**
+ * 阈值变色规则
+ */
+export class ThresholdColorFilter {
+  static filterRuleMapping = new Map([
+    ['equal', ThresholdColorFilter.equal],
+    ['notEqual', ThresholdColorFilter.notEqual],
+    ['test', ThresholdColorFilter.test],
+    ['notTest', ThresholdColorFilter.notTest],
+    ['gt', ThresholdColorFilter.gt],
+    ['gte', ThresholdColorFilter.gte],
+    ['lt', ThresholdColorFilter.lt],
+    ['lte', ThresholdColorFilter.lte]
+  ])
+
+  /**
+   * 判断是否相等
+   */
+  static equal ({ value, thresholdValue, thresholdColor }) {
+    if (value === thresholdValue) return thresholdColor
+  }
+
+  /**
+   * 判断是否不等
+   */
+  static notEqual ({ value, thresholdValue, thresholdColor }) {
+    if (value !== thresholdValue) return thresholdColor
+  }
+
+  /**
+   * 判断是否匹配正则表达式
+   */
+  static test ({ value, thresholdValue, thresholdColor }) {
+    try {
+      const regexp = new RegExp(thresholdValue)
+      if (regexp.test(value)) return thresholdColor
+    } catch (e) {
+      throw e
+    }
+  }
+
+  /**
+   * 判断是否不匹配正则表达式
+   */
+  static notTest ({ value, thresholdValue, thresholdColor }) {
+    try {
+      const regexp = new RegExp(thresholdValue)
+      if (!regexp.test(value)) return thresholdColor
+    } catch (e) {
+      throw e
+    }
+  }
+
+  /**
+   * 判断是否大于
+   */
+  static gt ({ value, thresholdValue, thresholdColor }) {
+    if (value > thresholdValue) return thresholdColor
+  }
+
+  /**
+   * 判断是否大于等于
+   */
+  static ge ({ value, thresholdValue, thresholdColor }) {
+    if (value >= thresholdValue) return thresholdColor
+  }
+
+  /**
+   * 判断是否小于
+   */
+  static lt ({ value, thresholdValue, thresholdColor }) {
+    if (value < thresholdValue) return thresholdColor
+  }
+
+  /**
+   * 判断是否小于等于
+   */
+  static le ({ value, thresholdValue, thresholdColor }) {
+    if (value <= thresholdValue) return thresholdColor
+  }
+
+  constructor ({ filters = [] }) {
+    this.filters = filters
+  }
+
+  calculateColor (value) {
+    const colors = []
+    const { filters, filterRuleMapping } = this
+    filters.forEach(({ rule, thresholdValue, thresholdColor }) => {
+      const filter = filterRuleMapping.get(rule)
+      filter && colors.push(filter({ value, thresholdValue, thresholdColor }))
+    })
+
+    return colors.filter(color => !!color)
+  }
+}
