d28ae73cba85aaf5fb595cc81cc4331dd921eddd
唐鸿鑫 <tanghx@puhuilink.com>
2020-08-20 16:03:05 +0800

    🚧(阈值规则) 规则列表渲染

--- src/components/Cmdb/index.js
+++ src/components/Cmdb/index.js
@@ -1,9 +1,17 @@
 import ModelTree from './ModelTree'
 import CmdbTree from './CmdbTree'
 import ComboSelect from './ComboSelect'
+import CmdbHostTypeSelect from './CmdbHostTypeSelect'
+import CmdbHostSelect from './CmdbHostSelect'
+import CmdbEndpointSelect from './CmdbEndpointSelect'
+import CmdbMetricSelect from './CmdbMetricSelect'
 
 export {
   ComboSelect,
   ModelTree,
-  CmdbTree
+  CmdbTree,
+  CmdbHostTypeSelect,
+  CmdbHostSelect,
+  CmdbEndpointSelect,
+  CmdbMetricSelect
 }
--- src/views/alarm/config/modules/AlarmStrategySchema/index.vue
+++ src/views/alarm/config/modules/AlarmStrategySchema/index.vue
@@ -2,7 +2,7 @@
   <a-modal
     centered
     :title="title"
-    :width="720"
+    :width="920"
     wrapClassName="AlarmStrategy__modal"
     v-model="visible"
     :afterClose="reset"
@@ -24,20 +24,83 @@
     <a-spin :spinning="spinning">
       <a-form-model ref="ruleForm" :model="formModel" :rules="formRules">
 
+        <!-- / 阈值名称 -->
         <a-form-model-item label="阈值名称" v-bind="formItemLayout" prop="name">
           <a-input v-model.trim="formModel.name" />
         </a-form-model-item>
 
-        <a-list :dataSource="formModel.exprs.opts">
-          <a-list-item slot="renderItem" slot-scope="item">
-            <a-icon slot="actions" type="close" />
-            <a-list-item-meta>
-              <p slot="title">
-                {{ item.operator }}{{ item.threshold }}
-              </p>
-            </a-list-item-meta>
-          </a-list-item>
-        </a-list>
+        <!-- / 数据域 -->
+
+        <!-- / 监控对象 -->
+
+        <!-- / 监控实体 -->
+        <!-- <CmdbEndpointSelect /> -->
+
+        <!-- / 检查项 -->
+        <!-- <CmdbMetricSelect /> -->
+
+        <!-- / 阈值计算条件 -->
+
+        <!-- / 阈值条件 -->
+        <a-row
+          class="ant-form-item"
+          v-for="(opt, index) in formModel.exprs.opts"
+          :key="index"
+        >
+          <a-col v-bind="formItemLayout.labelCol" class="ant-form-item-label">
+            <label title="阈值条件">阈值条件</label>
+          </a-col>
+
+          <a-col v-bind="formItemLayout.wrapperCol">
+            <a-row>
+              <a-col :span="8">
+                <a-form-model-item class="fw" v-bind="nestedFormItemLayout">
+                  <a-select
+                    class="fw"
+                    placeholder="等于（或大于、小于...）"
+                    v-model="opt.operator"
+                  >
+                    <a-select-option
+                      v-for="operator in ['=', '>', '>=', '<', '<=']"
+                      :key="operator"
+                      :value="operator"
+                    >{{ operator }}</a-select-option>
+                  </a-select>
+                </a-form-model-item>
+              </a-col>
+
+              <a-col :span="5">
+                <a-form-model-item v-bind="nestedFormItemLayout">
+                  <a-input-number v-model="opt.threshold" />
+                </a-form-model-item>
+              </a-col>
+
+              <a-col :span="9">
+                <a-form-model-item label="告警级别" v-bind="nestedFormItemLayout">
+                  <a-select
+                    class="fw"
+                    v-model="opt.alarm_level"
+                  >
+                    <a-select-option
+                      v-for="level in [1, 2, 3, 4, 5]"
+                      :key="level"
+                      :value="level"
+                    >{{ `L${level}` }}</a-select-option>
+                  </a-select>
+                </a-form-model-item>
+              </a-col>
+
+              <a-col :span="2">
+                <a-form-model-item>
+                  <a-button
+                    v-show="formModel.exprs.opts.length > 1"
+                    @click="removeExpressionOpts(index)"
+                  >删除</a-button>
+                </a-form-model-item>
+              </a-col>
+            </a-row>
+          </a-col>
+        </a-row>
 
       </a-form-model>
     </a-spin>
@@ -49,11 +112,15 @@
 import Schema from '@/components/Mixins/Modal/Schema'
 import { StrategyService } from '@/api-hasura/index'
 import _ from 'lodash'
+import { CmdbEndpointSelect, CmdbMetricSelect } from '@/components/Cmdb'
 
 export default {
   name: 'AlarmStrategySchema',
   mixins: [Schema],
-  components: {},
+  components: {
+    CmdbEndpointSelect,
+    CmdbMetricSelect
+  },
   props: {},
   data: () => ({
     formModel: {
@@ -62,9 +129,14 @@ export default {
         opts: []
       }
     },
+    // TODO: responsive
     formItemLayout: {
       labelCol: { span: 5 },
       wrapperCol: { span: 15, offset: 1 }
+    },
+    nestedFormItemLayout: {
+      labelCol: { span: 8 },
+      wrapperCol: { span: 13, offset: 2 }
     },
     spinning: false,
     submitLoading: false
@@ -120,6 +192,9 @@ export default {
         this.submitLoading = false
       }
     },
+    removeExpressionOpts (index) {
+      this.formModel.exprs.opts.splice(index, 1)
+    },
     async update () {
       try {
         this.submitLoading = true
@@ -140,6 +215,11 @@ export default {
 
 <style lang="less">
 .AlarmStrategy__modal {
+  .ant-modal-body {
+    height: 500px;
+    overflow: auto;
+  }
+
   .content {
     margin-top: 24px;
   }
