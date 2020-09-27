1cab8729cd6bcae5396836e6f29cc42e4e7e3f16
唐鸿鑫 <tanghx@puhuilink.com>
2020-09-27 14:13:52 +0800

    🐛 (极坐标图) 切换数据类型后未更新图表显示数据

--- src/model/charts/PolarChart.js
+++ src/model/charts/PolarChart.js
@@ -115,10 +115,12 @@ export default class PolarChart extends Chart {
       }
       case SOURCE_TYPE_NULL: {
         dbDataConfig.resetData()
+        this.chart.clear()
         break
       }
       case SOURCE_TYPE_ALARM:
       case SOURCE_TYPE_REAL: {
+        this.chart.clear()
         const {
           legend: dynamicLegend,
           series: dynamicSeries,
