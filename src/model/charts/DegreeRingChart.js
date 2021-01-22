26a8d7137f1b3cd4c9a92e56fcdfdf9eb5a8b2a1
唐鸿鑫 <tanghx@puhuilink.com>
2021-01-22 16:13:50 +0800

    🚧 (视图新接口) 环度数据结构对接

--- src/model/charts/DegreeRingChart.js
+++ src/model/charts/DegreeRingChart.js
@@ -5,6 +5,7 @@
 import Chart from './index'
 import { DEGREE_TYPE_HEALTH_DEGREE, DEGREE_TYPE_HEALTH_RING } from '../config/proprietaryConfigs/DegreeRingProprietaryConfig'
 import {
+  SOURCE_TYPE_COMBO,
   SOURCE_TYPE_NULL,
   SOURCE_TYPE_REAL,
   SOURCE_TYPE_STATIC
@@ -31,8 +32,9 @@ export default class DegreeRingChart extends Chart {
         formatter = `${staticData}`
         break
       }
+      case SOURCE_TYPE_COMBO:
       case SOURCE_TYPE_REAL: {
-        const dynamicData = await dataConfig.dbDataConfig.getOption(loadingDynamicData)
+        const dynamicData = await dataConfig.dbDataConfig.getOption(loadingDynamicData, sourceType)
         // console.log(dynamicData)
         formatter = `${dynamicData}`
         break
