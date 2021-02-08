accd428231990f446418f813850a4c6f7b551355
xzliang <yeyuyixiao@163.com>
2021-02-08 10:12:03 +0800

    ⚡️系统设置----工作组管理----多选 / 编辑/删除/分配用户/分配管理员/更改状态/分配权限

--- src/views/system/group/index.vue
+++ src/views/system/group/index.vue
@@ -49,9 +49,9 @@
       <template #operation>
         <a-button @click="onAdd" v-action:M0106>新增</a-button>
         <a-button @click="onEdit" :disabled="!hasSelectedOne" v-action:M0101>编辑</a-button>
-        <a-button @click="onBatchDelete" :disabled="!isSelectedValid" v-action:M0108>删除</a-button>
-        <a-button @click="onAllocateUser" :disabled="!isSelectedValid" v-action:M0101>分配用户</a-button>
-        <a-button @click="onAllocateAdmin" :disabled="!isSelectedValid" v-action:M0101>分配管理员</a-button>
+        <a-button @click="onBatchDelete" :disabled="!hasSelectedOne" v-action:M0108>删除</a-button>
+        <a-button @click="onAllocateUser" :disabled="!hasSelectedOne" v-action:M0101>分配用户</a-button>
+        <a-button @click="onAllocateAdmin" :disabled="!hasSelectedOne" v-action:M0101>分配管理员</a-button>
         <a-button @click="onToggleFlag" :disabled="!hasSelectedOne" v-action:M0101>更改状态</a-button>
         <a-button @click="onAllocateAuth" :disabled="!hasSelectedOne" v-action:M0110>分配权限</a-button>
       </template>
