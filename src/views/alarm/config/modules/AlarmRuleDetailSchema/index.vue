f2f1c4f36a0dc30e84ace0c9025b293f195c589a
woxiangjingjingdiaomin <39614866+woxiangjingjingdiaomin@users.noreply.github.com>
2021-02-25 16:56:59 +0800

    ⚡️告警管理---告警配置---告警规则 --- 查看 代码优化

--- src/views/alarm/config/modules/AlarmRuleDetailSchema/index.vue
+++ src/views/alarm/config/modules/AlarmRuleDetailSchema/index.vue
@@ -25,7 +25,7 @@
 
             <div class="AlarmRuleDetailSchema__item">
               <label class="AlarmRuleDetailSchema__item-label">监控类型</label>
-              <span class="AlarmRuleDetailSchema__item-value">{{ model.dictType == null ? '暂无数据' : model.dictType.value_label }}</span>
+              <span class="AlarmRuleDetailSchema__item-value">{{ model.dictType ? model.dictType.value_label :'暂无数据' }}</span>
             </div>
 
             <div class="AlarmRuleDetailSchema__item">
@@ -40,17 +40,17 @@
 
             <div class="AlarmRuleDetailSchema__item">
               <label class="AlarmRuleDetailSchema__item-label">设备名称</label>
-              <span class="AlarmRuleDetailSchema__item-value">{{ model.dictBrand == null ? '暂无数据' :model.dictBrand.value_label }}</span>
+              <span class="AlarmRuleDetailSchema__item-value">{{ model.dictBrand ? model.dictBrand.value_label : '暂无数据' }}</span>
             </div>
 
             <div class="AlarmRuleDetailSchema__item">
               <label class="AlarmRuleDetailSchema__item-label">监控实体</label>
-              <span class="AlarmRuleDetailSchema__item-value">{{ model.modelEndpoint == null ? '暂无数据': model.modelEndpoint.alias }}</span>
+              <span class="AlarmRuleDetailSchema__item-value">{{ model.modelEndpoint ? model.modelEndpoint.alias :'暂无数据' }}</span>
             </div>
 
             <div class="AlarmRuleDetailSchema__item">
               <label class="AlarmRuleDetailSchema__item-label">检查项</label>
-              <span class="AlarmRuleDetailSchema__item-value">{{ model.modelMetric == null ? '暂无数据': model.modelMetric.alias }}</span>
+              <span class="AlarmRuleDetailSchema__item-value">{{ model.modelMetric ? model.modelMetric.alias :'暂无数据' }}</span>
             </div>
           </div>
         </a-tab-pane>
