fb43142225e7941ec81abe7831039bde58020b76
Forest <971661136@qq.com>
2021-05-26 15:17:14 +0800

    数据平台对接

---
+++ src/components/Iframe/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe width="100%" height="100%" ref="childIframe" :src="`http://10.201.22.41:8899${link}`" frameborder="0"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'Iframe',
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  },
+  methods () {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/prediction/capacity/Summary/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Iframe link="/capacity/summary"></Iframe>
+</template>
+
+<script>
+import Iframe from '~~~/Iframe/index'
+export default {
+  name: 'Summary',
+  components: {
+    Iframe
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
