ef8e941c580dc08b2b281074df3b0952ce2a7502
Forest <971661136@qq.com>
2021-06-25 15:39:28 +0800

    巡更细节处理

--- src/views/system/user/modules/UserSchema.vue
+++ src/views/system/user/modules/UserSchema.vue
@@ -233,11 +233,11 @@ export default {
       flag: [
         {
           name: '有效',
-          value: 1
+          value: '1'
         },
         {
           name: '无效',
-          value: 0
+          value: '0'
         }
       ]
     },
