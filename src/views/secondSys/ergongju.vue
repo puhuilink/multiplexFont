975205df063d4d2d9a59a31797c37abd093b144e
Forest <971661136@qq.com>
2021-09-27 15:33:55 +0800

    二公局 路桥

---
+++ src/views/secondSys/ergongju.vue
@@ -0,0 +1,13 @@
+<template>
+  <Iframe width="100%" height="100%" src="https://10.201.20.84/ergongju/show/" style="border: none"></Iframe>
+</template>
+
+<script>
+export default {
+  name: 'ergongju'
+}
+</script>
+
+<style scoped>
+
+</style>
