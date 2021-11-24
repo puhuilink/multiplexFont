afdd38f20da63df4895cb0b353c227d83d865046
changw <changw98@126.com>
2021-11-24 11:22:24 +0800

    视图数据配置新接口中适配top x

--- src/views/design/modules/config/common/ComboDataSource.vue
+++ src/views/design/modules/config/common/ComboDataSource.vue
@@ -7,6 +7,7 @@
         <a-select-option value="1">性能数据</a-select-option>
         <a-select-option value="6">健康度</a-select-option>
         <a-select-option value="2">业务系统</a-select-option>
+        <a-select-option value="8">Top数据</a-select-option>
       </a-select>
     </a-form-item>
 
@@ -20,13 +21,40 @@
 
     <GroupSelect @change="change()" />
 
-    <a-form-item label="top">
+    <a-form-item label="top" :required="comboConfig.dataType === '8'">
       <a-input-number
         class="fw"
         v-model.number="comboConfig.top"
         :min="0"
         :parser="parserInt"
       ></a-input-number>
+    </a-form-item>
+    <a-form-item label="指标名称" v-if="comboConfig.dataType === '8'" :required="comboConfig.dataType === '8'">
+      <a-input
+        class="fw"
+        v-model="comboConfig.topContent.metricAlias"
+      ></a-input>
+    </a-form-item>
+    <a-form-item label="设备类型" v-if="comboConfig.dataType === '8'" :required="comboConfig.dataType === '8'">
+      <a-select
+        class="fw"
+        v-model="comboConfig.topContent.hostType"
+        mode="multiple"
+      >
+        <a-select-option v-for="(type,index) in hType" :key="index" :value="type.HostType">{{ type.HostType }}</a-select-option>
+      </a-select>
+    </a-form-item>
+    <a-form-item label="数据来源" v-if="comboConfig.dataType === '8'" :required="comboConfig.dataType === '8'">
+      <a-select
+        class="fw"
+        v-model="comboConfig.topContent.location"
+        mode="multiple"
+      >
+        <a-select-option value="XM1">厦门1</a-select-option>
+        <a-select-option value="XM2">厦门2</a-select-option>
+        <a-select-option value="BJ1">北京1</a-select-option>
+        <a-select-option value="BJ2">北京2</a-select-option>
+      </a-select>
     </a-form-item>
 
     <RefreshTime />
@@ -62,6 +90,7 @@ import CascaderHostEndpointMetricList from '~~~/CascaderHostEndpointMetricList'
 import BusinessSystemTreeHealthList from '~~~/BusinessSystemTreeHealthList'
 import BusinessSystemAllList from '~~~/BusinessSystemAllList'
 import { parserInt } from '@/utils/util'
+import { CmdbService } from '@/api'
 
 export default {
   name: 'ComboDataSource',
@@ -74,7 +103,8 @@ export default {
   props: {},
   data () {
     return {
-      visible: false
+      visible: false,
+      hType: []
     }
   },
   computed: {
@@ -82,13 +112,27 @@ export default {
       return this.config.dataConfig.dbDataConfig.comboConfig
     }
   },
+  created () {
+    this.fetchHostType()
+  },
   methods: {
-    parserInt
+    parserInt,
+    async fetchHostType () {
+      const result = await CmdbService.hostFind({
+        fields: [
+          'HostType:host_type'
+        ],
+        distinct: 'host_type'
+      })
+      const { data: { t_cmdb_host } } = result
+      this.hType = t_cmdb_host
+      console.log(this.hType)
+    }
   }
 }
 </script>
 
-<style lang="less">
+<style lang='less'>
 .ComboDataSource {
 
   &__modal {
