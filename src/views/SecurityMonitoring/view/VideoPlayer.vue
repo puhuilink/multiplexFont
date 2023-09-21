5563a566e4c346504c0519b6f71978c0d443cc3a
zhang <473284772@qq.com>
2023-09-21 14:27:18 +0800

    视频

--- src/views/SecurityMonitoring/view/VideoPlayer.vue
+++ src/views/SecurityMonitoring/view/VideoPlayer.vue
@@ -8,51 +8,39 @@
       :footer="null"
       :height="600"
       @cancel="handleCancel">
-      <video ref="videoPlayer" controls></video>
+      <a-spin :spinning="loading">
+        <iframe width="100%" height="500px" :src="url"></iframe>
+      </a-spin>
     </a-modal>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'VideoPlayer',
-
   data () {
     return {
       visible: false,
-      websocket: null,
-      activeKey: ''
-      // url: process.env.VUE_APP_QUOTE_URL + '/static/h5player/webplayer.html?cameraUrl=ws://10.201.53.20:559/openUrl/RbHmFck'
+      activeKey: '',
+      url: '',
+      loading: false
     }
   },
   methods: {
     handleCancel (e) {
       this.visible = false
       // 关闭视频流
-      this.websocket.close()
     },
-    childMethod (url, title) {
+    async childMethod (url, title) {
       // 创建WebSocket连接
+      this.loading = true
+
       console.log('socket', url)
       this.activeKey = title
-      this.websocket = new WebSocket('ws://10.201.53.20:559/openUrl/6J4mUOk')
-
-      // 监听WebSocket连接打开事件
-      this.websocket.onopen = () => {
-        console.log('WebSocket连接已打开')
-      }
-
-      // 监听WebSocket消息事件
-      this.websocket.onmessage = (event) => {
-        // 将WebSocket消息流附加到<video>元素上
-        this.$refs.videoPlayer.src = URL.createObjectURL(event.data)
-      }
-
-      // 监听WebSocket关闭事件
-      this.websocket.onclose = () => {
-        console.log('WebSocket连接已关闭')
-      }
+      this.url = `${process.env.VUE_APP_QUOTE_URL}/static/h5player/webplayer.html?cameraUrl=${url}`
+      setTimeout(() => {
+        this.loading = false
+      }, 1000)
     }
 
   }
